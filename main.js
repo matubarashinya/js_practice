b35b5568acfa6eaf6f5e42250295aa4271d1dd29
matubarashinya <is2baseball1130@icloud.com>
2020-12-13 22:50:32 +0900

    dom操作の練習

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const el = document.querySelector('.animate-title');
+  const str = el.innerHTML.trim();
+  let concatStr = '';
+
+  for(let c of str){
+    concatStr += `<span class= "char">${c}</span>`;
+  }
+  console.log(concatStr);
+});
